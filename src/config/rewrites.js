9c26d13cbfda8530c221da585fc5a85910f0c04c
pvlach <vlach1989@gmail.com>
2020-03-09 10:19:29 +0100

    update homepage to publish on github pages

--- src/config/rewrites.js
+++ src/config/rewrites.js
@@ -11,7 +11,7 @@ let tacrAgritasGeoserverWmsUrl = 'http://45.56.96.184:8965/geoserver/agritas/wms
 
 if (window.location.hostname === 'gisat.github.io') {
 	tacrAgritasDataRepositoryUrl = 'https://cors-anywhere.herokuapp.com/http://project.gisat.cz/agritas/static/data/';
-	tacrAgritasGeoserverWmsUrl = 'http://45.56.96.184:8965/geoserver/agritas/wms/http://project.gisat.cz/agritas/static/data/';
+	tacrAgritasGeoserverWmsUrl = 'https://cors-anywhere.herokuapp.com/http://45.56.96.184:8965/geoserver/agritas/wms';
 } else if (window.location.hostname === 'localhost') {
 	tacrAgritasDataRepositoryUrl = 'http://localhost:3031/agritas/static/data/';
 	tacrAgritasGeoserverWmsUrl = 'http://localhost:3032/geoserver/agritas/wms';
